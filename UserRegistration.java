bae2fd170ea2250b75add192550eb2007f2cc069
venkatajayadeep <jayadeep101097@gmail.com>
2021-08-06 09:25:52 +0530

    Added : MINCAPTHREE

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,26 @@
 package bridgelabz;
+import java.util.regex.*;
+import java.util.Scanner;
 public class UserRegistration {
-	public static void main(String[] args) {
-		System.out.println("WelCome");
+	 private boolean check;
+	    private String firstName;
+	    Scanner sc = new Scanner(System.in);
+	    
+	    public void checkFirstName() {
+	        System.out.println("enter first name starts with cap and has minimum 3 character : ");
+	        firstName = sc.nextLine();
+	        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
+	        if (!check) {
+	            System.out.println(" first name is invalid ");
+	            checkFirstName();
+	        }
+	    	else {
+	            System.out.println(" first name is valid ");
+	        }
+	    }
 
-	}
+	    public static void main(String args[]) {
+	        UserRegistration user = new UserRegistration();
+	        user.checkFirstName();
+	    }
 }
