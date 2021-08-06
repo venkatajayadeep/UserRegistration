12ac3a914d498f388199fd8c30c367f0eac4f933
venkatajayadeep <jayadeep101097@gmail.com>
2021-08-06 09:30:23 +0530

    Added : LastName

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,8 +2,8 @@ package bridgelabz;
 import java.util.regex.*;
 import java.util.Scanner;
 public class UserRegistration {
-	 private boolean check;
-	    private String firstName;
+	   private boolean check;
+	    private String firstName, lastName;
 	    Scanner sc = new Scanner(System.in);
 	    
 	    public void checkFirstName() {
@@ -19,8 +19,22 @@ public class UserRegistration {
 	        }
 	    }
 
+	    public void checkLastName() {
+	        System.out.println("enter last name starts with cap and has minimum 3 character : ");
+	        lastName = sc.nextLine();
+	   	    check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
+	   	    if (!check) {
+	   		     System.out.println(" last name is invalid ");
+	   		     checkLastName();
+	   	    }
+	   	    else
+	   		     System.out.println(" last name is valid ");
+	    }
+
 	    public static void main(String args[]) {
+		
 	        UserRegistration user = new UserRegistration();
 	        user.checkFirstName();
+	        user.checkLastName();
 	    }
 }
